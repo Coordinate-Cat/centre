5410b6aac2870e127bfabbabf878de1d66f694e3
Coordinate-Cat <tetrapasta02@gmail.com>
2021-03-29 15:34:29 +0900

    change : cms microCMS → contentful

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
-require('dotenv').config();
-import axios from 'axios'
-const { X_API_KEY } = process.env;
+const { getConfigForKeys } = require('./lib/config.js')
+const ctfConfig = getConfigForKeys([
+  'CTF_BLOG_POST_TYPE_ID',
+  'CTF_SPACE_ID',
+  'CTF_CDA_ACCESS_TOKEN'
+])
+
+const { createClient } = require('./plugins/contentful.js')
+const cdaClient = createClient(ctfConfig)
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
@@ -22,28 +28,20 @@ export default {
     ]
   },
 
-  privateRuntimeConfig: {
-    apiKey: X_API_KEY
+  generate: {
+    routes() {
+      return cdaClient
+        .getEntries(ctfConfig.CTF_BLOG_POST_TYPE_ID)
+        .then(entries => {
+          return [...entries.items.map(entry => `/blog/${entry.fields.slug}`)]
+        })
+    }
   },
-  publicRuntimeConfig: {
-    apiKey: process.env.NODE_ENV !== 'production' ? X_API_KEY : undefined
+  env: {
+    CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
+    CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID
   },
-
-  // generate: {
-  //   async routes({ $config }) {
-  //     const { pages } = await axios.get(
-  //       'https://centre.microcms.io/api/v1/centre?limit=100',
-  //       { headers: { 'X-API-KEY': $config.apiKey } }
-  //       )
-  //       .then((res) =>
-  //         res.data.contents.map((content) => ({
-  //           route: `/${content.id}`,
-  //           payload: content
-  //         }))
-  //       )
-  //     return pages
-  //   }
-  // },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
--- pages/index.vue
+++ pages/index.vue
@@ -1,85 +1,34 @@
 <template>
-  <div class="
-    h-screen
-    flex
-    font-monos
-    font-bold
-  bg-black">
-    <div class="
-      w-72
-      p-10
-      pt-11
-      text-center
-      text-white">
-      <h1>CENTRE</h1>
-    </div>
-    <div class="w-full my-10 mx-5">
-      <ul class="flex flex-wrap">
-        <li class="pr-4 pt-6" v-for="content in sortedItemsByName" :key="content.id">
-          <nuxt-link class="
-            w-24
-            h-24
-            p-1
-            block
-            border-4
-            line-clamp-1
-            rounded-sm
-            hover:animate-ping
-            hover:bg-white
-            hover:border-white
-            hover:text-black
-            text-white" :to="`/${content.id}`">
-            {{ content.title }}
-          </nuxt-link>
-        </li>
-      </ul>
-      <details class="text-white pt-10">
-        <summary class="
-        w-56
-        p-4
-        border-4
-        rounded-sm">kernelmode</summary>
-        <details>
-        <summary>Site Archive</summary>
-        <a target="_blank" href="/archive/kernelmode-info/index.html">kernelmode.info</a>
-        </details>
-        <details>
-        <summary>Complete KernelMode.info Download</summary>
-        <a target="_blank" href="/archive/kernelmode-info.7z">KernelMode.info Download</a>
-        </details>
-      </details>
-    </div>
-  </div>
+  <section class="index">
+    <card
+      v-for="(post,i ) in posts"
+      :key="i"
+      :title="post.fields.title"
+      :id="post.sys.id"
+      :date="post.sys.updatedAt"
+    />
+  </section>
 </template>
 
 <script>
-import axios from 'axios'
+import Card from '~/components/card.vue'
+import { createClient } from '~/plugins/contentful.js'
+
+const client = createClient()
 export default {
-  async asyncData({ $config, error }) {
-    try {
-      const { data } = await axios.get(
-        'https://centre.microcms.io/api/v1/centre',
-        { headers: { 'X-API-KEY': $config.apiKey } }
-      )
-      return data
-    } catch (err) {
-      error ({
-        statusCode: err.response ? err.response.status : 500,
-        message: err.message,
-      });
-    }
+  transition: 'slide-left',
+  components: {
+    Card
   },
-  computed: {
-    // reverseContents() {
-    //     return this.contents.slice().reverse();
-    // },
-    sortedItemsByName(){
-        return this.contents.sort((a, b) => {
-          let textA = a.title.toUpperCase();
-          let textB = b.title.toUpperCase();
-          return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-        });;
-    },
+  asyncData({ env, params }) {
+    return client
+      .getEntries(env.CTF_BLOG_POST_TYPE_ID)
+      .then(entries => {
+        return {
+          posts: entries.items
+        }
+      })
+      .catch(console.error)
   }
 }
 </script>
